671f465ae8177b1ba7e42d19c69aee37021ac863
Chinmay K <52078442+chinmaykh@users.noreply.github.com>
2022-07-11 20:51:10 +0530

    Website Beta

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ var app = angular.module("quarks", []);
 
 // Article Class
 class Article {
-  constructor(title, author, url, col_width) {
-    this.title = title;
+  constructor(title, author = chance.name(), url = chance.url(), words = 3) {
+    if (!title) this.title = chance.sentence({ words });
+    this.title = chance.sentence({ words: words });
     this.author = author;
     this.url = url;
-    this.col_width = col_width;
   }
 }
 
@@ -16,50 +16,58 @@ app.controller("homeController", [
   ($scope, $http) => {
     console.log("Quarks Controller Loaded...");
 
-    $scope.articles = [
-      new Article(
-        "The Evolution of JavaScript",
-        "John Resig",
-        "http://ejohn.org/blog/the-evolution-of-javascript/",
-        6
-      ),
-      new Article(
-        "A Beginner's Guide to ES6",
-        "John Resig",
-        "https://johnresig.com/blog/es6-in-depth/",
-        6
-      ),
-      new Article(
-        "JavaScript Performance",
-        "John Resig",
-        "http://ejohn.org/blog/javascript-performance/",
-        6
-      ),
-      new Article(
-        "Why We Don't Use 'new'",
-        "John Resig",
-        "http://ejohn.org/blog/why-we-dont-use-new-for-javascript/",
-        6
-      ),
-      new Article(
-        "The Rise of ES6",
-        "John Resig",
-        "http://ejohn.org/blog/the-rise-of-es6-part-1/",
-        6
-      ),
-      new Article(
-        "The Rise of ES6",
-        "John Resig",
-        "http://ejohn.org/blog/the-rise-of-es6-part-2/",
-        6
-      ),
-      new Article(
-        "The Rise of ES6",
-        "John Resig",
-        "http://ejohn.org/blog/the-rise-of-es6-part-3/",
-        6
-      ),
-    ];
+    // Generate 20 Articles Inline
+    $scope.articles = [];
+    for (let i = 0; i < 20; i++) {
+      let words = Math.floor(Math.random()*5);
+      console.log(words);
+      $scope.articles.push(new Article(words = 3));
+    }
+
+    $scope.main_articles = $scope.articles.slice(0, 3);
+    $scope.side_articles = $scope.articles.slice(4, 16);
+
+    $http({
+      method: "GET",
+      url: "https://www.googleapis.com/blogger/v3/blogs/6405308807706472057/posts",
+      params: {
+        key: "AIzaSyCHz4IbV1hPghJomm9OPibseRtVrHSo7AI",
+        fields:"items(title,author,url,id)",
+      },
+    }).then(
+      (res) => {
+        // $scope.articles = res.data.items.map((blogger_article) => {
+        //   console.table(blogger_article);
+        //   return new Article(
+        //     title= blogger_article.title,
+        //     author= blogger_article.author.displayName,
+        //     url= `/article.html?id=${blogger_article.id}`,
+        //   )
+        // });
+        console.log($scope.articles);
+        
+        // Layouting
+        $scope.SPM = 6;
+        
+        // Initiate JS controlled CSS after entire page loads
+
+        var parallax_elems = document.querySelectorAll('.parallax');
+        var parallax_instances = M.Parallax.init(parallax_elems, {});
+
+        var scrollspy_elems = document.querySelectorAll('.scrollspy');
+        var scrollspy_instances = M.ScrollSpy.init(scrollspy_elems, {});
+
+
+      },
+      (err) => {
+        alert("Unable to fetch data!");
+      }
+    );
+
+    $scope.getSideArticles = (index) => {
+      let SPM = $scope.SPM;
+      return $scope.side_articles.slice(SPM * index, SPM * (index + 1));
+    };
   },
 ]);
 
@@ -70,17 +78,21 @@ app.controller("articleController", [
   ($scope, $http, $sce) => {
     console.log("Article Controller Loaded...");
 
+    // Get Article ID from URL
+    let id = window.location.href.split("?")[1].split("=")[1];
+    console.log(id);
+
     $http({
       method: "GET",
-      url: "https://www.googleapis.com/blogger/v3/blogs/6405308807706472057/posts",
+      url: "https://www.googleapis.com/blogger/v3/blogs/6405308807706472057/posts/" + id,
       params: {
         key: "AIzaSyCHz4IbV1hPghJomm9OPibseRtVrHSo7AI",
       },
     }).then(
       (res) => {
-        console.table(res.data.items);
+        console.table(res.data);
         // Lets work with one article... We shall sanitize and inject the html
-        $scope.article = res.data.items[1];
+        $scope.article = res.data;
         $scope.article.body = $sce.trustAsHtml($scope.article.content);
       },
       (err) => {
@@ -93,8 +105,9 @@ app.controller("articleController", [
 // What CSS cannot do
 
 // Make navbar visible after scrolling only
-window.onscroll = ()=>{
+window.onscroll = () => {
   // Make opacity propotional to scroll position
-  let opacity = (window.scrollY / window.innerHeight).toFixed(2);
-  document.querySelector("nav").style.opacity = opacity;  
-}
+  let opacity = (window.scrollY/ window.innerHeight).toFixed(2);
+  document.getElementById("toc").style.opacity = opacity;
+  document.querySelector("nav").style.opacity = opacity;
+};
---
+++ homepage.md
@@ -0,0 +1,17 @@
+# Quarks Homepage
+
+This page serves as the verbal outline of what we wanna accomplish. This document needs changing anytime a change in the page is made.
+
+## Landing
+The quarks logo does a little boogey. We're thining about autoscrolling after the dance is done
+
+## Articles Sections
+
+### News section
+This section will hold some important articles. We shall have `n` very important articles interspersed with `m` important articles. These articles will be picked from the last in the queue. 
+
+### Archives and Search
+This section will hold a centralised search bar and shall exit to a new page with search results. Option to search by author name.
+
+## Just before the section
+We have one more piece of premium web estate. The team can decide what to implement in this region.
